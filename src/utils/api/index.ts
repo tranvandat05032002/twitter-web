e03cfcfbb32d952a9775388b12708f20d79cd764
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-08-14 12:08:57 +0700

    feat: config axios

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,9 +1,1 @@
-import axios from "axios";
-export const apiInstance = axios.create({
-  baseURL: "http://localhost:4000/",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    "Cache-Control": "no-cache",
-  },
-});
+
