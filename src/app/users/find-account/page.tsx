5e566ab0cc412dd1bd52e7ed0b6a1e539f898602
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-08-26 16:10:20 +0700

    feat: split find account page

--- src/app/users/find-account/page.tsx
+++ src/app/users/find-account/page.tsx
@@ -1,117 +1,14 @@
 "use client";
-import { useRouter } from "next/navigation";
-import {
-  ERROR_FORM_MESSAGES,
-  ErrorMessage,
-  GhostButton,
-  Input,
-  LayoutAuth,
-  PrimaryButton,
-} from "@/components/common";
-import React from "react";
-import { TwitterIcon } from "@/components/SingleUseComponents";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { isObjectEmpty } from "@/utils/handlers";
-import { useAuth } from "@/store";
-import {
-  getEmailCookies,
-  removeEmailCookies,
-  saveEmailCookies,
-} from "@/utils/auth/cookies";
-import { Routers } from "@/utils/router/routers";
+import { LayoutAuth } from "@/components/common";
+import FindAccountPage from "@/components/layouts/FindAccount";
 export interface ForgotForm {
   email: string;
 }
 const FindEmail = () => {
-  const [canSubmit, setCanSubmit] = React.useState<boolean>(true);
-  const router = useRouter();
-  const { findEmail } = useAuth((state) => state);
-  const schemaValidator = yup.object().shape({
-    email: yup
-      .string()
-      .required(ERROR_FORM_MESSAGES.emailRequired)
-      .email(ERROR_FORM_MESSAGES.isEmail),
-  });
-  const {
-    control,
-    handleSubmit,
-    getValues,
-    formState: { errors, isSubmitting },
-  } = useForm<ForgotForm>({
-    resolver: yupResolver(schemaValidator),
-    mode: "onSubmit",
-    defaultValues: {
-      email: "",
-    },
-    context: { canSubmit },
-  });
-  React.useEffect(() => {
-    if (isObjectEmpty(getValues())) {
-      setCanSubmit(true);
-    }
-    setCanSubmit(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [canSubmit]);
-  const handleCancel = () => {
-    router.push(Routers.signInPage);
-  };
-  const handleFindEmail = async (values: ForgotForm) => {
-    if (isObjectEmpty(values)) return;
-    saveEmailCookies(values.email);
-    const response = await findEmail(values);
-    if (response?.status === 200) {
-      router.push(Routers.forgotPasswordPage);
-    } else {
-      removeEmailCookies();
-    }
-  };
   return (
     <>
       <LayoutAuth>
-        <div className="flex items-center justify-center">
-          <TwitterIcon size="small"></TwitterIcon>
-        </div>
-
-        <form onSubmit={handleSubmit(handleFindEmail)} autoComplete="off">
-          <div>
-            <div>
-              <h1 className="text-3xl font-bold pb-2 text-center">
-                Tìm tài khoản Twitter của bạn
-              </h1>
-              <p className="text-base text-[#71767B] font-light">
-                Nhập email được liên kết với tài khoản của bạn để thực hiện thay
-                đổi mật khẩu
-              </p>
-            </div>
-            <div className="py-[13px]">
-              <Input
-                control={control}
-                placeholder="Email"
-                type="email"
-                name="email"
-              ></Input>
-              {errors && <ErrorMessage>{errors.email?.message}</ErrorMessage>}
-            </div>
-          </div>
-          <PrimaryButton
-            className={`w-full py-[12px] text-base rounded-full  mt-6 mb-4 px-8 ${
-              canSubmit ? "hover:bg-none" : ""
-            }`}
-            type="submit"
-            isLoading={isSubmitting}
-            disabledForm={canSubmit}
-          >
-            Tiếp theo
-          </PrimaryButton>
-        </form>
-        <GhostButton
-          className={`w-full py-[12px] text-base rounded-full  mb-6 mt-4 px-8`}
-          onClick={handleCancel}
-        >
-          Hủy
-        </GhostButton>
+        <FindAccountPage></FindAccountPage>
       </LayoutAuth>
     </>
   );
